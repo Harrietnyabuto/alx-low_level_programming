efb4a5b0a02d54a68aaf1096e8b54a6de7482191
Harrietnyabuto <nharriet@kabarak.ac.ke>
2023-07-29 14:12:46 -0700

    debbugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,17 +1,18 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef MAIN_H
 
-int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+#define MAIN_H
 
-#endif
+
+#include <stdio.h>
+
+
+void positive_or_negative(int i);
+
+int largest_number(int, int, int);
+
+void print_remaining_days(int, int, int);
+
+int convert_day(int, int);
+
+
+#endif /* MAIN_H */
